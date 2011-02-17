6c6de5e8d8861939512b203476994fbc1b17168c
Jonathan <jon@acss.net.au>
2011-02-17 12:46:13 +1000

    The meat of the issue: it works, but the URL is incorrect.

--- lib/overview_forward_plugin_patch.rb
+++ lib/overview_forward_plugin_patch.rb
@@ -28,7 +28,7 @@ module OverviewForwardPluginPatch
                     end
                     classes = (ancestors.empty? ? 'root' : 'child')
                     s << "<li class='#{classes}'><div class='#{classes}'>" +
-                         link_to_project(project, {:action => 'issues'}, :class => "project #{User.current.member_of?(project) ? 'my-project' : nil}")
+                         link_to_project(project, {:action => 'issues', :id => project, :controller=> 'projects'}, :class => "project #{User.current.member_of?(project) ? 'my-project' : nil}")
                     s << "<div class='wiki description'>#{textilizable(project.short_description, :project => project)}</div>" unless project.description.blank?
                     s << "</div>\n"
                     ancestors << project
